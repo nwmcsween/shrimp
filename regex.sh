0984c7955e74edce9f6164652dce19f007ec1d19
Nathan McSween <nwmcsween@gmail.com>
2018-01-18 22:35:23 +0000

    tabs -> 2 spaces; compact

--- regex.sh
+++ regex.sh
@@ -1,14 +1,6 @@
 rx_match() {
-    needle="$1"; haystack="$2"
+  needle="$1"; haystack="$2"
 
-    eval \
-        "case \"\$haystack\" in
-            $needle)
-                return 0
-            ;;
-            *)
-                return 1
-            ;;
-        esac";
+  eval "case \"\$haystack\" in $needle) return 0;; *) return 1;; esac";
 }
 
