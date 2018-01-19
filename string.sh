1c5ad7dbc8215f521d41b79cee6a7ee8f5eb1bb6
Nathan McSween <nwmcsween@gmail.com>
2018-01-19 22:06:22 +0000

    string: add idempotent push

--- string.sh
+++ string.sh
@@ -9,4 +9,18 @@ str_to_sha1() {
   return $?
 }
 
+str_ipush() {
+  dst=$1; src=$2; sep=$3
+
+  eval "test -z \$$dst" && sep=
+ 
+  if eval "rx_match \"${sep}${src}|*${sep}${src}${sep}*|$src\" \"\${$dst}\""; then
+    return 1
+  fi
+
+  eval "$dst=\"\$$dst\$sep\$src\""
+
+  return 0
+}
+
 fi
