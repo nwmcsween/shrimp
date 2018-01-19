9d989eece9babd3d959f55fa3ade6b240acd836d
Nathan McSween <nwmcsween@gmail.com>
2018-01-19 21:03:05 +0000

    file.sh - remove trailing semicolon, remove extra $ in arith

--- file.sh
+++ file.sh
@@ -2,9 +2,9 @@ file_tmp() {
   src=$1; dir=$2; ext=$3
 
   set -C
-  i=0;
+  i=0
 
-  while [ "$i" -le 50 ]; do i=$(($i + 1))
+  while [ "$i" -le 50 ]; do i=$((i + 1))
     if eval "$src=\"\${dir}/tmp-\$\$-\$PPID-\$i.\$ext\" 2>|\
        /dev/null > \"\${dir}/tmp-\$\$-\$PPID-\$i.\$ext\""; then
       i=0
@@ -17,4 +17,3 @@ file_tmp() {
 
   test "$i" -eq 0 && return 0 || return 1
 }
-
